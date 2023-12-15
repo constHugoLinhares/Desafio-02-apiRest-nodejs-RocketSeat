aa0ae3c5ab7a590e8d5056ee42e0fc2e2b24f77a
Hugo Linhares <hugolinhares92@gmail.com>
2023-12-14 21:35:57 -0300

    configs of knexFile to deploy

---
+++ src/routes/meals.ts
@@ -0,0 +1,25 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
+
+export async function mealsRoutes(app: FastifyInstance) {
+    app.get(
+        '/',
+        {
+            preHandler: [checkSessionIdExists],
+        },
+        async (request) => {
+            const { sessionId } = request.cookies
+
+            const transactions = await knex('meals')
+               .where('session_id', sessionId)
+               .select()
+
+        return { transactions }
+        },
+    )
+
+    
+}
