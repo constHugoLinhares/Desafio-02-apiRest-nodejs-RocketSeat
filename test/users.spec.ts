ea192172280a5e1936455fa389f4d2912bdde4eb
Hugo Linhares <hugolinhares92@gmail.com>
2024-01-28 18:44:25 -0300

    fix:users spec migrations

--- test/users.spec.ts
+++ test/users.spec.ts
@@ -1,6 +1,7 @@
-import { afterAll, beforeAll, describe, it } from 'vitest';
+import { afterAll, beforeAll, beforeEach, describe, it } from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app';
+import { execSync } from 'child_process';
 
 describe('userRoutes', () => {
   beforeAll(async () => {
@@ -9,6 +10,10 @@ describe('userRoutes', () => {
     
   afterAll(async () => {
     await app.close();
+  });
+
+  beforeEach(() => {
+    execSync('npm run migrate:latest');
   });
 
   it('Should be possible create a new user', async () => {
